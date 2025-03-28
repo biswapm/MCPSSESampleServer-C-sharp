f9fcf62785fe2f8563bd9c3abb347a126dd4edb2
Pujarini Mohapatra <29349597+biswapm@users.noreply.github.com>
2025-03-28 15:44:15 +0530

    Sample SSE server using Official Microsoft C-sharp SDK

---
+++ AspNetCoreSampleSseServer/Program.cs
@@ -0,0 +1,37 @@
+using AspNetCoreSampleSseServer;
+using ModelContextProtocol;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMcpServer()
+    .WithToolsFromAssembly();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddSingleton(_ =>
+{
+    var client = new HttpClient() { BaseAddress = new Uri("https://api.weather.gov") };
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weather-tool", "1.0"));
+    return client;
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapMcpSse();
+
+
+
+
+app.Run();
+
