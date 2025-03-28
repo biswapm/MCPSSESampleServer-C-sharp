ff058237e0ba65294bf7845a4b59bab7676810bc
Pujarini Mohapatra <29349597+biswapm@users.noreply.github.com>
2025-03-28 15:49:47 +0530

    NA

--- AspNetCoreSampleSseServer/obj/Debug/net8.0/AspNetCoreSampleSseServer.AssemblyInfo.cs
+++ AspNetCoreSampleSseServer/obj/Debug/net8.0/AspNetCoreSampleSseServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreSampleSseServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fcf62785fe2f8563bd9c3abb347a126dd4edb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreSampleSseServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreSampleSseServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
